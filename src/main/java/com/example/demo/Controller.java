737bf093e1e09b236849ceb3c23e7ee41e0c78f2
angeloskoroli <akoroli25@scienceleadership.org>
2023-12-01 10:21:22 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Controller {
+    //fields for your data
+
+    //fields for your controls
+    
+    //1 initialized method
+
+    //Method for all onActions
+
+}
---
+++ src/main/java/com/example/demo/GUILauncher.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class GUILauncher extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(GUILauncher.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
